3eda378f2c9d00ed4c500e9b50b5470d483bd0ad
Mario <mario5555@cox.net>
2018-03-28 19:03:35 -0700

    snake

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 var grid;
-var numrows = 10;
-var numcolumns = 20;
-var rowHeight = 50;
-var columnWidth = 50;
+var numrows = 50;
+var numcolumns = 50;
+var rowHeight = 20;
+var columnWidth = 20;
 
 function setup() {
-  createCanvas(1500, 1500);
-  //var seed = Math.floor(random(2));
-  //randomSeed(seed);
-  //console.log("seed:", seed)
+  createCanvas(1000, 1000);
   grid = [];
   for (var row = 0; row < numrows; row++) {
     grid[row] = [];
@@ -16,7 +13,7 @@ function setup() {
       grid[row][col] = false;
     }
   }
-  grid[3][3] = true;
+  grid[1][1] = true;
 }
 
 function static() {
@@ -38,24 +35,98 @@ function interesting() {
   var bottomright = grid[row+1] && grid[row+1][col+1];
   var side = top || left || right || bottom;
   var corner = topleft || topright || bottomleft || bottomright;
-  if ((top || left || right || bottom) && !(topleft && top && left) && !(top && topright && right) && !(left && bottomleft && bottom) && !(right && bottom && bottomright)){
+  if ((top || left || right || bottom) && !(topleft && top && left) && !(top && topright && right) && !(left && bottomleft && bottom) && !(right && bottom && bottomright)) {
     grid[row][col] = true;
   }
-  //console.log("Row: ", row);
-  //console.log("Column: ", col);
+}
+
+function dab() {
+  var topleft = grid[row-1] && grid[row-1][col-1];
+  var top = grid[row-1] && grid[row-1][col];
+  var topright = grid[row-1] && grid[row-1][col+1];
+  var left = grid[row][col-1];
+  var right = grid[row][col+1];
+  var bottomleft = grid[row+1] && grid[row+1][col-1];
+  var bottom = grid[row+1] && grid[row+1][col];
+  var bottomright = grid[row+1] && grid[row+1][col+1];
+  var side = top || left || right || bottom;
+  var corner = topleft || topright || bottomleft || bottomright;
+  if (top) {
+    random([row++, col++]);
+  }
+  if (left) {
+    random([row--, col++, row++]);
+  }
+  if (bottom) {
+    random([row--, col++]);
+  }
+  grid[row][col] = true;
+  console.log("Row: ", row);
+  console.log("Column: ", col);
+}
+
+row = 1;
+col = 2;
+
+function snake() {
+  var topleft = grid[row-1] && grid[row-1][col-1];
+  var top = grid[row-1] && grid[row-1][col];
+  var topright = grid[row-1] && grid[row-1][col+1];
+  var left = grid[row][col-1];
+  var right = grid[row][col+1];
+  var bottomleft = grid[row+1] && grid[row+1][col-1];
+  var bottom = grid[row+1] && grid[row+1][col];
+  var bottomright = grid[row+1] && grid[row+1][col+1];
+  var side = top || left || right || bottom;
+  var corner = topleft || topright || bottomleft || bottomright;
+  grid[row][col] = true;
+  if (top) {
+    if ((random(["Right", "Down"]) == "Down") && (row < numrows-1) && !(bottomleft)) {
+      row++;
+    }
+    else {
+      col++;
+    }
+  }
+  if (left) {
+    var direction = random(["Up", "Right", "Down"])
+    if ((direction == "Up") && (row > 0) && !(topleft)) {
+      row--;
+    }
+    else if ((direction == "Down") && (row < numrows-1) && !(bottomleft))  {
+      row++;
+    }
+    else {
+      col++;
+    }
+  }
+  if (bottom) {
+    if ((random(["Up", "Right"]) == "Up") && (row > 0) && !(topleft)) {
+      row--;
+    }
+    else {
+      col++;
+    }
+  }
+  if (col > numcolumns) {
+    throw new Error('Program end');
+  }
 }
 
 function draw() {
   //static();
-  interesting();
+  //interesting();
+  //dab();
+  snake();
   //console.log(grid);
   grid.forEach(function (row, y) {
     row.forEach(function (cell, x) {
       if (cell) {
-        fill(250, 250, 250);
+        fill(random(256), random(256), random(256));
       }
       else {
-        fill(60, 60, 60);
+        fill(256, 256, 256);
+        noStroke();
       }
       rect(x*rowHeight, y*columnWidth, rowHeight, columnWidth);
     })
